80bd7d6a999b71a8962e08cfbaf6918596816662
Pawel Zareba <zareba.pawel@gmail.com>
2021-05-04 23:40:23 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,36 @@
-input.onGesture(Gesture.Shake, function () {
-    maqueen.motorStopAll()
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+basic.showIcon(IconNames.Happy)
+let turnSpeed = 50
+let forwardSpeed = 100
 let strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.White))
 basic.forever(function () {
-    strip.showColor(neopixel.colors(NeoPixelColors.White))
     if (0 == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
-        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, forwardSpeed)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, forwardSpeed)
     } else {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
-            maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+            maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, turnSpeed)
             if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
                 maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
-                maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+                maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, turnSpeed)
             }
         } else {
             if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-                maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+                maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, turnSpeed)
                 maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
                 if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-                    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+                    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, turnSpeed)
                     maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
                 }
                 if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-                    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+                    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, turnSpeed)
                 } else {
                     maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
                 }
